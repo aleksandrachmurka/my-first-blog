f9ecdfcafe7ba869861904a2e8f76e60b729221a
ola <aleksandrakrzywicka@gmail.com>
2017-09-16 14:29:52 +0200

    My Django Girls app, first commit

---
+++ bieganie.py
@@ -0,0 +1,11 @@
+temperatura = 20
+if temperatura == 20:
+	print("Biegamy!")
+elif temperatura > 25:
+	print("Włącz wentylator. proszę.")
+elif temperatura <= 10:
+	print("Biegamy, ale lepiej złóżmy bluzy.")
+elif temperatura < -10:
+	print("Nigdzie nie idę!")
+else:
+	print("Sprawdż temperaturę")
